5e81b18d3bfc2c900978f0c5af74a6eef39d2eb6
Awaisjeeh <m.awais.imran.36@gmail.com>
2024-08-21 22:07:19 +0500

    Initial commit

---
+++ websiteScraper/Program.cs
@@ -0,0 +1,48 @@
+﻿//using HtmlAgilityPack;
+//using System;
+//using System.Net.Http;
+//using System.IO;
+//using System.Security.Cryptography.X509Certificates;
+//namespace WebScraper
+//{
+//    class Program
+//    {
+//        static void Main(String[] args)
+//        {
+//            // Send get request to ucp.edu.pk
+//            String url = "https://ucp.edu.pk/";
+//            var httpClient = new HttpClient();
+//            var html = httpClient.GetStringAsync(url).Result;
+//            var htmlDocument = new HtmlDocument();
+//            htmlDocument.LoadHtml(html);
+
+//            // Get the dean message
+//            var MessageElement = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"content\"]/article/div[3]/div/div[1]/div/div/div/div[2]/div[2]/p/text()");
+//            var message = MessageElement.InnerText.Trim();
+
+//            Console.WriteLine("Chairman Message from Ucp: " + message);
+//            writeCSV(message);
+
+//        }
+//        public static string[] readCSV(string url)
+//        {
+
+//        }
+
+//        private static void writeCSV(string message)
+//        {
+
+//            try
+//            {
+
+//                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"filepath.txt", true))
+//                {
+//                    file.WriteLine(message);
+//                }
+//            }
+//            catch (Exception ex) {
+//                throw new Exception("Error in file: ", ex);
+//            }
+//            }
+//    }
+//}
---
+++ websiteScraper/WebScrapper.cs
@@ -0,0 +1,93 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Path to your CSV file containing URLs
+            string csvFilePath = "urls.txt";
+
+            // Read URLs from the CSV file
+            var urls = readCSV(csvFilePath);
+
+            foreach (var url in urls)
+            {
+                try
+                {
+                    // Send GET request to each URL
+                    var httpClient = new HttpClient();
+                    var html = httpClient.GetStringAsync(url).Result;
+                    var htmlDocument = new HtmlDocument();
+                    htmlDocument.LoadHtml(html);
+
+                    // Get the Dean's message
+                    var MessageElement = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"content\"]/article/div[3]/div/div[1]/div/div/div/div[2]/div[2]/p");
+                    if (MessageElement != null)
+                    {
+                        var message = MessageElement.InnerText.Trim();
+                        Console.WriteLine("Chairman Message from Ucp: " + message);
+
+                        // Write the message to a file
+                        writeCSV(message);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Message element not found for URL: " + url);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+        public static List<string> readCSV(string filePath)
+        {
+            var urls = new List<string>();
+
+            try
+            {
+                using (var reader = new StreamReader(filePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(line))
+                        {
+                            urls.Add(line.Trim());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading CSV file: " + ex.Message);
+            }
+
+            return urls;
+        }
+
+        private static void writeCSV(string message)
+        {
+            try
+            {
+                using (StreamWriter file = new StreamWriter(@"output.txt", true))
+                {
+                    file.WriteLine(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error writing to file: " + ex.Message);
+            }
+        }
+    }
+}
